23ab7ba68eaf6193182237139284184bf2956076
Siavash <siavash@192-168-1-6.tpgi.com.au>
2016-09-26 20:14:12 +1000

    first commit

---
+++ Package.swift
@@ -0,0 +1,16 @@
+import PackageDescription
+
+let package = Package(
+    name: "Microservice2",
+    targets: [
+        Target(name: "Microservice2", dependencies: [])
+    ],
+    dependencies: [
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 1, minor: 7),
+        .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 14),
+        .Package(url: "https://github.com/IBM-Bluemix/cf-deployment-tracker-client-swift.git", majorVersion: 0, minor: 4)
+    ],
+    exclude: ["Makefile", "Package-Builder"]
+)
